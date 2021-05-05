f35618ae3f11d857e57333cab4a6d859c12ca7bd
Grupo5s <50368073+Grupo5s@users.noreply.github.com>
2021-05-05 15:06:37 -0300

    Adicionado componente GeneralStatusBar color para resolver problema de exibição da StatusBar no Iphone

--- App.js
+++ App.js
@@ -15,10 +15,12 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import Routes from './src/routes';
 import { persistor, store } from './src/store';
+import GeneralStatusBarColor from './src/component/GeneralStatusBarColor';
 
 const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <GeneralStatusBarColor backgroundColor="#BCBF00" barStyle="light-content"/>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <Routes />
